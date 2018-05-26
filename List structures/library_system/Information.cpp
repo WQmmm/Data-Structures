e9c6b1d163efff2b9df28e4a042c3cc8b4e2f6a9
WQ <36372552+WQmmm@users.noreply.github.com>
2018-05-26 09:15:05 +0800

    Information.h

--- List structures/library_system/Information.cpp
+++ List structures/library_system/Information.cpp
@@ -1,5 +1,6 @@
 ///////////Information.cpp////////////
 #include<stdio.h>
+#include<string.h>
 #include"Information.h"
 
 Information::Information()             //构造函数
@@ -21,9 +22,10 @@ Information::~Information()            //析构函数
 	}
 }
 
-void Information::creatlist(int n)     //初始化函数
+void Information::creatlist()       //初始化函数
 {
-	Node *p,*q;						  //定义指针p
+	Node *p,*q;			    		//定义指针p
+	int n;
 	printf("请输入要存的书籍的数量:");
 	scanf("%d",&n);
 	p=head;
@@ -48,14 +50,44 @@ void Information::insert()        //插入函数
 
 }
 
-void Information::search()        //查找函数
+int Information::search()        //查找函数
 {
-
+	Node *p;
+	char book[SIZE];
+	p=head;
+	printf("请输入要查找的书名或作者名:");
+    scanf("%s",&book);
+	while(p!=NULL)
+	{
+		if(strcmp(book,p->book_name)==0 || strcmp(book,p->author_name)==0)                      //查找成功
+		{
+			return 1;          
+		}
+		p=p->next;
+	}
+	return 0;
 }
 
-void Information::remove()        //删除函数
+int Information::remove()        //删除函数
 {
-
+	Node *p,*q;
+	p=head;
+	q=p->next;
+	char book[SIZE];
+	printf("请输入要删除的书籍:");
+	scanf("%s",&book);
+	while(q!=NULL)
+	{
+		if(strcmp(book,q->book_name)==0 || strcmp(book,q->author_name)==0)             //查找成功
+		{	
+			p->next=q->next;
+			delete q;
+			return 1;
+		}
+		p=q;
+		q=q->next;
+	}
+   	return 0;
 }
 
 void Information::display()       //显示函数
@@ -68,7 +100,6 @@ void Information::display()       //显示函数
 		printf("作者姓名:%s\n",p->author_name);
 		printf("总库数量:%d\n",p->total_count);
 		p=p->next;
-	
 	}
 	printf("\n");
 }
