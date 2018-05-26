6bfda8f8aee496702b7fab187ae7cd40861f90ca
WQ <36372552+WQmmm@users.noreply.github.com>
2018-05-26 09:13:13 +0800

    main.cpp

--- List structures/library_system/main.cpp
+++ List structures/library_system/main.cpp
@@ -1,12 +1,47 @@
 #include<stdio.h>
-#include"Information.h"
+#include"node.h"
 
 int main()
 {
 	Information Info;
-	int n;
-	Info.creatlist(n);
-	Info.display();
+	int c;
+	Info.creatlist();
+	printf("初始化成功!");
+	printf("1.显示图书信息    2.查找图书    3.删除图书    0.退出\n");
+	while(1)
+	{
+	
+		printf("请选择所要进行的操作:");
+		scanf("%d",&c);
+		switch(c)
+		{
+		case 0:
+			break;
+		case 1:
+			Info.display();
+			break;
+		case 2:
+			int x;
+			x=Info.search();
+			if(x==1)
+				printf("查找成功!\n");
+			else
+				printf("该书籍不存在!\n");
+			break;
+		case 3:
+			int y;
+			x=Info.remove();
+			if(x==1)
+				printf("删除成功!\n");
+			else
+				printf("删除失败!\n");
+			break;
+		default:
+			printf("输入有误，请重新输入!\n");
+		}
+		if(c==0)
+			break;
+	}
 	return 0;
 }
 
